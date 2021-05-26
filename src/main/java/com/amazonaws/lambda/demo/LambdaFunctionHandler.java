aa6b31bfbca5101c79f647271ea452dcce39c18a
kenduraghav <kenduraghav@gmail.com>
2021-05-26 20:40:47 +0800

    AWS Lambda demo  initial code

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,14 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> {
+
+    
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Received input: " + input);
+        return "Hello World - " + input;
+    }
+}
